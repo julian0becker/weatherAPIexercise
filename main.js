6fb3f6ddbbaa5b90b4e80c3cb5544482827a1b30
julian0becker <julianentwickelt@gmail.com>
2018-11-22 15:19:06 +0100

    pic search functionality up and running

--- main.js
+++ main.js
@@ -31,3 +31,35 @@ function convertKelvin(kelvin) {
 function renderOutput(container, celsius, weatherIcon, city) {
   container.innerHTML = `<img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather in ${city}"> The temperature in ${city} is currently ${celsius}°C.`;
 }
+
+let citySearch = document.getElementById("city");
+citySearch.addEventListener("click", searchImage);
+
+function searchImage() {
+  fetch("https://api.teleport.org/api/urban_areas/?embed=ua:item/ua:images")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let cityInput = document.getElementById("input").value;
+
+      let url = function() {
+        for (i = 0; i < data["_embedded"]["ua:item"].length; i++) {
+          if (data["_embedded"]["ua:item"][i]["name"] == cityInput) {
+            return data["_embedded"]["ua:item"][i]["_embedded"]["ua:images"][
+              "photos"
+            ]["0"]["image"]["mobile"];
+          }
+        }
+      };
+      console.log(url());
+      renderImage(url());
+    });
+}
+
+function renderImage(url) {
+  let container = document.getElementById("imageContainer");
+  let image = document.createElement("div");
+  container.appendChild(image);
+  image.innerHTML = `<img class="card-img-top" src="${url}" alt="Card image cap" />`;
+}
